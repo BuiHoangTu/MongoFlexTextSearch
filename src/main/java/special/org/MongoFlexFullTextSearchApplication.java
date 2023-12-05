16d1f43b5500dc6e55eef4287fcffc9b49b44f9e
tu <bhtu.work@gmail.com>
2023-12-05 15:16:38 +0700

    init

---
+++ src/main/java/special/org/MongoFlexFullTextSearchApplication.java
@@ -0,0 +1,13 @@
+package special.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoFlexFullTextSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoFlexFullTextSearchApplication.class, args);
+    }
+
+}
