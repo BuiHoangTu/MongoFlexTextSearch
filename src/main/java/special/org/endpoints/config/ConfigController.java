168ef9e5ef4d4f4ce4829eb367ce9e05cc79efcc
tu <bhtu.work@gmail.com>
2024-01-30 11:39:56 +0700

    adding config changes

---
+++ src/main/java/special/org/endpoints/config/ConfigController.java
@@ -0,0 +1,28 @@
+package special.org.endpoints.config;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import special.org.configs.ResourceWatching;
+
+@RestController
+@RequestMapping(path = "api/config")
+public class ConfigController {
+    private final ResourceWatching resourceWatching;
+
+    public ConfigController(ResourceWatching resourceWatching) {
+        this.resourceWatching = resourceWatching;
+    }
+
+    @GetMapping(path = "watching/get")
+    public ResponseEntity<ResourceWatching> getResourceWatching() {
+        return ResponseEntity.ok(resourceWatching);
+    }
+
+//    @PostMapping(path = "watching/get")
+//    public ResponseEntity setResourceWatching(ResourceWatching resourceWatching) {
+//        resourceWatching
+//    }
+}
